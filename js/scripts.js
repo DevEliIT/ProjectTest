34d07baee97c113aa77e0e226e1d4451bf322d48
Eliabe dos Santos Oliveira <72847444+eliabedossantos@users.noreply.github.com>
2023-07-23 22:16:11 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the progress bar fill element
+    const progressBarFill = document.querySelector(".progressBarFill");
+
+    // Set the final width to 90%
+    progressBarFill.style.width = "90%";
+});
